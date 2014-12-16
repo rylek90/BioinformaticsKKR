6cbef960fa211c2a3aa0da766b74740654a7fbde
rylek90 <rylek90@gmail.com>
2014-12-16 21:52:22 +0100

    AlignPage - added SequencesLists + alignment of alignment page

--- BioinformaticsKKR/View/AlignView.xaml.cs
+++ BioinformaticsKKR/View/AlignView.xaml.cs
@@ -1,21 +1,43 @@
 ﻿using System.Windows.Controls;
 using BioinformaticsKKR.Core.DependencyInjection;
+using BioinformaticsKKR.Provider;
 using BioinformaticsKKR.ViewModel;
+using FirstFloor.ModernUI.Windows;
+using FirstFloor.ModernUI.Windows.Navigation;
 
 namespace BioinformaticsKKR.View
 {
     /// <summary>
     /// Interaction logic for AlignView.xaml
     /// </summary>
-    public partial class AlignView : Page
+    public partial class AlignView : Page, IContent
     {
-        private IAlignViewModel _viewModel;
+        private readonly IAlignViewModel _viewModel;
 
         public AlignView()
         {
             InitializeComponent();
             MainGrid.DataContext = ContainerBootstrap.Container.GetInstance<IAlignViewModel>();
             _viewModel = MainGrid.DataContext as IAlignViewModel;
+        }
+
+        public void OnFragmentNavigation(FragmentNavigationEventArgs e)
+        {
+            
+        }
+
+        public void OnNavigatedFrom(NavigationEventArgs e)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _viewModel.FirstSequencesList = SequencesRepository.Instance.Sequences;
+            _viewModel.SecondSequencesList = SequencesRepository.Instance.Sequences;
+        }
+
+        public void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
         }
     }
 }
--- BioinformaticsKKR/ViewModel/AlignViewModel.cs
+++ BioinformaticsKKR/ViewModel/AlignViewModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using Bio;
 using Bio.SimilarityMatrices;
 using BioinformaticsKKR.Core.ViewModel;
 using BioinformaticsKKR.Service.Alignement;
@@ -15,8 +16,13 @@ namespace BioinformaticsKKR.ViewModel
         SimilarityMatrix.StandardSimilarityMatrix CurrentSimilarityMatrix { get; set; }
         List<IAlignSequences> SequencesAligners { get; }
         IAlignSequences CurrentAligner { get; set; }
+        ISequence FirstSequenceSelected { get; set; }
+        ISequence SecondSequenceSelected { get; set; }
         void OnPropertyChanged(string propertyName);
         event PropertyChangedEventHandler PropertyChanged;
+
+        IEnumerable<ISequence> FirstSequencesList { get; set; }
+        IEnumerable<ISequence> SecondSequencesList { get; set; }
     }
 
     public class AlignViewModel : ViewModelBase, IAlignViewModel
@@ -25,6 +31,10 @@ namespace BioinformaticsKKR.ViewModel
         private IAlignSequences _currentAligner;
         private SimilarityMatrix.StandardSimilarityMatrix _currentSimilarityMatrix;
         private readonly IStatusViewModel _statusService;
+        private IEnumerable<ISequence> _secondSequencesList;
+        private IEnumerable<ISequence> _firstSequencesList;
+        private ISequence _secondSequenceSelected;
+        private ISequence _firstSequenceSelected;
 
         public string Status
         {
@@ -41,9 +51,7 @@ namespace BioinformaticsKKR.ViewModel
             )
         {
             _statusService = statusService;
-            _statusService.PropertyChanged += (sender, e) => { 
-                OnPropertyChanged(e.ToString());
-            };
+            _statusService.PropertyChanged += (sender, e) => OnPropertyChanged(e.ToString());
             _sequencesAligners = sequencesAligners;
         }
 
@@ -73,5 +81,44 @@ namespace BioinformaticsKKR.ViewModel
             set { _currentAligner = value; OnPropertyChanged("CurrentAligner"); }
         }
 
+        public IEnumerable<ISequence> FirstSequencesList
+        {
+            get { return _firstSequencesList; }
+            set
+            {
+                _firstSequencesList = value;
+                OnPropertyChanged("FirstSequenceList");
+            }
+        }
+
+        public ISequence FirstSequenceSelected
+        {
+            get { return _firstSequenceSelected; }
+            set
+            {
+                _firstSequenceSelected = value;
+                OnPropertyChanged("FirstSequenceSelected");
+            }
+        }
+
+        public IEnumerable<ISequence> SecondSequencesList
+        {
+            get { return _secondSequencesList; }
+            set
+            {
+                _secondSequencesList = value;
+                OnPropertyChanged("SecondSequenceList");
+            }
+        }
+
+        public ISequence SecondSequenceSelected
+        {
+            get { return _secondSequenceSelected; }
+            set
+            {
+                _secondSequenceSelected = value;
+                OnPropertyChanged("SecondSequenceSelected");
+            }
+        }
     }
 }
